7f02eb4bce00c812f62f8345098207e6d7ee1dc9
Shahzad <shahzad.adil@e-zest.in>
2022-01-13 11:35:30 +1400

    More work on gulp

--- site/gulpfile.js
+++ site/gulpfile.js
@@ -5,24 +5,46 @@ const concat = require("gulp-concat");
 const htmlReplace = require("gulp-html-replace");
 const copy = require("gulp-copy");
 const cleanCss = require("gulp-clean-css");
+const concatCss = require("gulp-concat-css");
 
 function minifyJs() {
-    return src('content/js/*.js', 'content/app-scripts/*.js')
+    let sources = src(
+        'content/js/*.js',
+        //'node_modules/jquery/dist/jquery.min.js',
+        'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+    );
+
+    return sources
         .pipe(uglify())
         .pipe(concat('lib.min.js'))
         .pipe(dest('dist/js/'));
 }
 
 function minifyCss() {
-    return src('content/css/*.css')
+    let sources = src(
+        'node_modules/bootstrap/dist/css/bootstrap.css',
+        'content/css/custom.css'
+    );
+
+    return sources
         .pipe(cleanCss())
-        .pipe(concat('lib.min.css'))
+        .pipe(concatCss('dist/lib.min.css'))
         .pipe(dest('dist/css/'));
 }
 
 function copyImages() {
     return src('content/images/*.*')
         .pipe(dest('dist/images/'));
+}
+
+function copyFonts() {
+    return src('node_modules/bootstrap-icons/font/fonts/*')
+        .pipe(dest('dist/css/fonts/'));
+}
+
+function copyIcons() {
+    return src('node_modules/bootstrap-icons/icons/*')
+        .pipe(dest('dist/css/icons/'));
 }
 
 function updateHtml() {
@@ -33,4 +55,4 @@ function updateHtml() {
 }
 
 exports.default = this.minify;
-exports.minify = parallel(minifyJs, minifyCss, copyImages, updateHtml);
+exports.minify = parallel(minifyJs, minifyCss, copyImages, copyFonts,copyIcons, updateHtml);
